3827c6fa336fa77dec255cbf46569c51df3944e3
domjweb <dominiquejadonwebb@gmail.com>
2026-01-04 13:32:50 -0500

    Started JS practice folder by simply adding a React/TypeScript Vite instance to the repo

---
+++ 121625.js
@@ -0,0 +1,7 @@
+const ReportCard = ({data, theme, branding}) => {
+    const primaryColor, logoUrl } = branding;
+
+    return (
+        <div className="report-card" style={{borderColor: primaryColor}}></div>
+    )
+}
