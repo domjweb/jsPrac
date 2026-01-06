e10fd8daae7373aabe2ec574ec09004f4a79b4fe
domjweb <dominiquejadonwebb@gmail.com>
2026-01-06 12:31:30 -0500

    Started vanilla JavaScript routine with comments

--- 121625.js
+++
@@ -1,7 +0,0 @@
-const ReportCard = ({data, theme, branding}) => {
-    const primaryColor, logoUrl } = branding;
-
-    return (
-        <div className="report-card" style={{borderColor: primaryColor}}></div>
-    )
-}
---
+++ comments/1626.js
@@ -0,0 +1,6 @@
+// I am also starting up my vanilla JavaScript practice sessions
+
+/*
+I am starting these practice sessions by of course,
+making single line and multi-line comments
+*/
