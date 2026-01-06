0917471d39e86a07cbe7963009c7ba82f7a43e6d
domjweb <dominiquejadonwebb@gmail.com>
2026-01-06 15:48:29 -0500

    Started the same ritualistic practice routine for JavaScript and created space for comments and print statements

---
+++ print_statements/1626.js
@@ -0,0 +1,7 @@
+console.log("Here I am practicing printing in JavaScript")
+
+function printing() {
+    console.log("Here I am using a function to also print a statement in JavaScript")
+}
+printing()
+
